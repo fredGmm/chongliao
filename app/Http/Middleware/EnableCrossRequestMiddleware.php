5570a3b370cf8bcc15208f3093ca7cb51205ecff
fredGmm <572752023@qq.com>
2019-11-14 18:25:11 +0800

    跨域支持

--- app/Http/Middleware/EnableCrossRequestMiddleware.php
+++ app/Http/Middleware/EnableCrossRequestMiddleware.php
@@ -13,30 +13,45 @@ class EnableCrossRequestMiddleware
      * @param  \Closure  $next
      * @return mixed
      */
+//    public function handle($request, Closure $next)
+//    {
+//        $response = $next($request);
+//        $origin = $request->server('HTTP_ORIGIN') ? $request->server('HTTP_ORIGIN') : '*';
+//        $allow_origin = [
+//            'http://127.0.0.1:9528',
+//            'http://localhost:9528',
+//            'http://127.0.0.1:8080',
+//            'http://192.168.0.101:8080',
+//            'http://192.168.0.102:8080',
+//            'http://192.168.0.100:8080',
+//            'http://192.168.0.44:9528',
+//            'chongliaoweb.me'
+//        ];
+////        $headers = 'X-Requested-With, Content-Type, ' . join(', ', array_keys($request->headers));
+//        if (in_array($origin, $allow_origin) || true) {
+//            $response->header('Access-Control-Allow-Origin', '*');
+//            $response->header('Access-Control-Allow-Headers', 'Content-Type,XFILENAME,XFILECATEGORY,XFILESIZE,x-csrf-token,x-token,X-XSRF-TOKEN
+//            X-Requested-With, Content-Type, accept-language, accept-encoding, referer, user-agent, origin,Cookie, accept, connection, host');
+//
+//            $response->header('Access-Control-Expose-Headers', 'Authorization, authenticated');
+//            $response->header('Access-Control-Allow-Methods', 'GET,POST,PUT,OPTIONS,PATCH,DELETE,HEAD');
+//            $response->header('Content-Type', '*');
+//            $response->header('charset', 'utf-8');
+//            $response->header('Access-Control-Allow-Credentials', 'true');
+//        }
+//        return $response;
+//    }
+
     public function handle($request, Closure $next)
     {
         $response = $next($request);
         $origin = $request->server('HTTP_ORIGIN') ? $request->server('HTTP_ORIGIN') : '*';
-        $allow_origin = [
-            'http://127.0.0.1:9528',
-            'http://localhost:9528',
-            'http://127.0.0.1:8080',
-            'http://192.168.0.101:8080',
-            'http://192.168.0.102:8080',
-            'http://192.168.0.100:8080',
-            'http://192.168.0.44:9528',
-            'chongliaoweb.me'
-        ];
+        $response->header('Access-Control-Allow-Origin', $origin);
+        $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie,x-token, X-CSRF-TOKEN, Accept, Authorization, X-XSRF-TOKEN');
+        $response->header('Access-Control-Expose-Headers', 'Authorization, authenticated');
+        $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
+        $response->header('Access-Control-Allow-Credentials', 'true');
 
-        if (in_array($origin, $allow_origin) || true) {
-            $response->header('Access-Control-Allow-Origin', '*');
-            $response->header('Access-Control-Allow-Headers', 'Content-Type,XFILENAME,XFILECATEGORY,XFILESIZE,x-csrf-token,x-token,X-Requested-With, Content-Type, accept-language, accept-encoding, referer, user-agent, origin, accept, connection, host');
-            $response->header('Access-Control-Expose-Headers', 'Authorization, authenticated');
-            $response->header('Access-Control-Allow-Methods', 'GET,POST,PUT,OPTIONS,PATCH,DELETE,HEAD');
-            $response->header('Content-Type', '*');
-            $response->header('charset', 'utf-8');
-            $response->header('Access-Control-Allow-Credentials', 'true');
-        }
         return $response;
     }
 }
