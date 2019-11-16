5d44bdd8be53510c2dedff7d80cf0a9753529511
fredGmm <572752023@qq.com>
2019-11-16 16:02:20 +0800

    前置 处理

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
@@ -10,6 +11,16 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * @return $this
+     */
+    public function before(){
+        $method =request()->getMethod();
+        if (strtolower($method) == 'options') {
+            return response()->json([])->setEncodingOptions(JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+        }
+    }
 
     protected function jsonOk($data, $message = '', $code = 0)
     {
@@ -19,7 +30,6 @@ class Controller extends BaseController
             'message' => $message
         ];
         return response()->json($return)->setEncodingOptions(JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
-
 //        echo json_encode($return, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
     }
 
@@ -31,5 +41,11 @@ class Controller extends BaseController
             'data' => $data,
         ];
         return response()->json($return)->setEncodingOptions(JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+    }
+
+    public function callAction($method, $parameters)
+    {
+        $this->before();
+        return call_user_func_array([$this, $method], $parameters);
     }
 }
