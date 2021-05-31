e856bc578dd6994dc91435c6bcfb4e579d9c4c29
fredGmm <572752023@qq.com>
2021-05-31 23:41:51 +0800

    外包项目

---
+++ app/Models/DnArticleClass.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Support\Facades\Validator;
+
+class DnArticleClass extends Model
+{
+    use ValidatesRequests;
+    //
+    protected $table = 'tb_notice_class';
+    protected $connection = 'mysql_dn';
+    protected $rules = [
+
+    ];
+    protected $message = [
+
+    ];
+
+    public $timestamps = FALSE;
+    protected $fillable = ['name','zt'];
+    protected $appends = [];
+//    protected $hidden = ['path'];
+
+    public $errors;
+
+    public function validate($data)
+    {
+        // make a new validator object
+//        $v = Validator::make($data, $this->rules, $this->message);
+//
+//        // check for failure
+//        if ($v->fails()) {
+//            // set errors and return false
+//            $this->errors[] = $v->errors()->first();
+//            return false;
+//        }
+        return true;
+    }
+
+//    public function setPathAttribute()
+//    {
+//    }
+
+
+
+
+}
